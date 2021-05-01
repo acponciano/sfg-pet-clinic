265a2216b15b609af75e8278524b9fcb81c25a78
Arlindo <arlindo.ponciano@gmail.com>
2021-05-01 10:37:43 -0300

    Add contato info to owner. Closes #28.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -7,7 +7,34 @@ import java.util.Set;
  */
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Vet> vets;
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return this.telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Vet> getVets() {
         return this.vets;
