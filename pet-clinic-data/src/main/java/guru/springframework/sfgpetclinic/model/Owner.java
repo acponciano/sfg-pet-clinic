9aaeb55bbe431d1a51b8e8c2afeb25a399e26c5f
Arlindo <arlindo.ponciano@gmail.com>
2021-05-01 10:27:36 -0300

    Added visits, updt owners with pet. Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by jt on 7/13/18.
  */
 public class Owner extends Person {
+
+    private Set<Vet> vets;
+
+    public Set<Vet> getVets() {
+        return this.vets;
+    }
+
+    public void setVets(Set<Vet> vets) {
+        this.vets = vets;
+    }
 }
