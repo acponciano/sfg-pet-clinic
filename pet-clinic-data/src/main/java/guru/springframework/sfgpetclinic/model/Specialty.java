3eaee118dda7f3c86db0ea31035118e8ae245b1f
Arlindo <arlindo.ponciano@gmail.com>
2021-05-01 10:34:41 -0300

    added specialties, added to vet. Closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
