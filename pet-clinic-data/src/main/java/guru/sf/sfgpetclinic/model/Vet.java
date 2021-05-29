e44f79dfbee96773ad330198d9365cbc1042b484
acponciano <arlindo.ponciano@gmail.com>
2021-05-29 11:10:45 -0300

    Vet specialties constructor CircleCI fix.

--- pet-clinic-data/src/main/java/guru/sf/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/sf/sfgpetclinic/model/Vet.java
@@ -31,6 +31,11 @@ public class Vet extends Person {
         super(id, firstName, lastName);
     }
 
+    public Vet(Set<Specialty> specialties) {
+        super();
+        specialties = this.specialties;
+    }
+
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
