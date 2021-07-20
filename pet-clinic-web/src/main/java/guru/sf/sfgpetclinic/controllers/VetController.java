9a2a506f7b51a6fb0c6a594cf94e7caff23f1b1e
acponciano <arlindo.ponciano@gmail.com>
2021-07-20 09:10:31 -0300

    Vets JSON endpoint. Closes #66

--- pet-clinic-web/src/main/java/guru/sf/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/sf/sfgpetclinic/controllers/VetController.java
@@ -1,18 +1,21 @@
 package guru.sf.sfgpetclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import guru.sf.sfgpetclinic.model.Image;
 import guru.sf.sfgpetclinic.model.Vet;
 import guru.sf.sfgpetclinic.services.ImageService;
 import guru.sf.sfgpetclinic.services.VetService;
 
-@RequestMapping({ "/vets" })
 @Controller
 public class VetController {
 
@@ -24,7 +27,7 @@ public class VetController {
         this.imageService = imageService;
     }
 
-    @RequestMapping({ "", "/", ".html", "/index.html" })
+    @RequestMapping({ "/vets", "/vets/", "/vets.html", "/vets/index.html" })
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
@@ -32,27 +35,32 @@ public class VetController {
         return "vets/index";
     }
 
-    @RequestMapping({ "/find", "/find/index", "/find/index.html" })
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> listVetsJSON(Model model) {
+        return vetService.findAll();
+    }
+
+    @RequestMapping({ "/vets/find", "/vets/find/index", "/vets/find/index.html" })
     public String findVets(Model model) {
 
         return "notImplemented";
     }
 
-    @RequestMapping({ "/{id}/show" })
+    @RequestMapping({ "/vets/{id}/show" })
     public String showVet(Model model, @PathVariable Long id) {
         model.addAttribute("vet", vetService.findById(id));
 
         return "vets/show";
     }
 
-    @RequestMapping({ "/{id}/update" })
+    @RequestMapping({ "/vets/{id}/update" })
     public String updateVet(Model model, @PathVariable Long id) {
         model.addAttribute("vet", vetService.findById(id));
 
         return "vets/update";
     }
 
-    @RequestMapping({ "/{id}/saveImage/{imageId}" })
+    @RequestMapping({ "/vets/{id}/saveImage/{imageId}" })
     public String saveImageVet(Model model, @PathVariable Long id, @PathVariable Long imageId) {
 
         Vet vet = vetService.findById(id);
@@ -66,14 +74,14 @@ public class VetController {
         return "redirect:/vets";
     }
 
-    @RequestMapping({ "/add" })
+    @RequestMapping({ "/vets/add" })
     public String addVet(Model model) {
         model.addAttribute("vet", new Vet());
 
         return "vets/update";
     }
 
-    @PostMapping({ "", "/" })
+    @PostMapping({ "/vets", "/vets/" })
     public String saveOrUpdate(@ModelAttribute Vet vet) {
 
         Vet savedVet = vetService.save(vet);
